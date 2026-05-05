3fb3a678e3af4edb98317566395483da06379f20
tacaomta <tacao@mta.edu.vn>
2026-05-05 10:34:59 +0900

    Initial commit: Django + MSSQL setup

---
+++ users/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+class User(AbstractUser):
+    roles = models.ManyToManyField(Role, related_name="users")
+
+    def __str__(self):
+        return self.username
